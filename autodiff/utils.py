a35ec8868f57277f86e83e6740239f271e0b021a
Theo-MAC <tguenais@g.harvard.edu>
2019-11-15 16:00:14 -0500

    utils to handle the wrong input type

--- autodiff/utils.py
+++ autodiff/utils.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 #TODO-Implement the get_right shape for a list
 
 def get_right_shape(x):
@@ -9,14 +8,18 @@ def get_right_shape(x):
     type for the definition of a variable.
     If np.ndarray is two-dimensional, make it one dimensional.
     """
-    #TODO=Handle complex and other weird types
-    numeric_type = isinstance(x, (int, float, complex)) and not isinstance(x, bool)
+    #TODO-Handle complex and other weird types
+    numeric_type = isinstance(x, (int, float)) and not isinstance(x, bool)
     if numeric_type:
         return reshape_float(x)
     elif isinstance(x, np.ndarray):
         return reshape_array(x)
+    elif isinstance(x, list):
+        return reshape_array(np.array(x))
+    elif isinstance(x, tuple):
+        return reshape_array(np.array(x))
     else:
-        message = "Input is type {} and should be float or np.ndarray"
+        message = "Input is type {} and should be float, np.ndarray or list".format(type(x))
         raise TypeError(message)
         
 def reshape_array(x):
@@ -27,6 +30,11 @@ def reshape_array(x):
         message = "Evaluation point needs to be one-dimensional \
             found the following problem: {}".format(e)
         raise TypeError(message)
+    #We can also loop through an input and make sure that it's made of numeric types.
+    try:
+        out_x.dtype = np.float64
+    except Exception as e:
+        raise TypeError("The input contained some values that we could not convert to floating point numbers")
     
     if len(out_x) > max(x.shape): #We have a reshape a matrix into an array. Not wanted.
         raise TypeError("Input can not be  matrix. Input's original shape: {}".format(x.shape))
@@ -47,58 +55,11 @@ def reshape_float(x):
     assert (isinstance(out_x, np.ndarray)) and (len(out_x.shape) == 1)
     return out_x
 
-#TODO- THEO
+#TODO-THEO. or not needed with the array.
 def reshape_list(x):
     return None
 
-def first_tests():
-    x = np.array([[12,14]])
-    print(type(x))
-    try:
-        isinstance(x, list)
-    except Exception as e:
-        print(e)
-
-    #x = x.reshape(1)
-    print(x.shape)
-    print(x[0])
-    X = np.array(x)
-    print(x.shape)
-    print(type(x))
-    ####
-    print('Start')
-    x = np.array([[12]])
-    out_x = reshape_array(x)
-    print(x.shape)
-    print(out_x.shape)
-    ####
-    x = np.array([[12]])
-    out_x = reshape_array(x)
-    print(x.shape)
-    print(out_x.shape)
-    ###
-    ###
-    try:
-        x = np.array([[12, 14]])
-        out_x = reshape_array(x)
-    except Exception as e:
-        print(e)
-    print('DOne')
-    x=int(12)
-    XX = np.array(8)
-    print("TYPE XX", type(XX))
-    XX = XX.reshape(1,)
-    print("TYPE XX", type(XX))
-    out_x = reshape_float(x)
-    print(out_x.shape)
-    print(out_x)
-    print(type(out_x))
-    x = float(14)
-    out_x = reshape_float(x)
-    print(out_x.shape)
-    print(out_x)
-
-
+#First basics tests
 if __name__ == "__main__":
     x = np.array([[12,14]])
     print(x.shape, len(x))
@@ -106,12 +67,86 @@ if __name__ == "__main__":
     out_x = get_right_shape(x)
     print(type(out_x), out_x.shape)
     #x = np.array([[12]])
-    ##out_x = get_right_shape(x)
-    #print(type(out_x), out_x.shape)
+ 
+
+    #1-List of list
+    print("List of list test")
+    x = [[123, 34], [34, 26,38]]
+    test_function(get_right_shape,x)
+
+    #2-List of list where the second dimension is null
+    print("Handle the user mispecified list")
+    x = [[123, 34]]
+    test_function(get_right_shape, x)
+
+    #3-Correct list
+    x = [1000]
+    print('Simple one-dimensional list')
+    test_function(get_right_shape, x)
+
+    #4-Matrix-form Array
     x = np.array([[12, 14], [18, 36]])
+    print(x, x.dtype)
+    new_x = x
+    new_x.dtype = np.float64
+    print(new_x, new_x.dtype)
+    print("Matrix input")
+    test_function(get_right_shape, x)
+
+    #5-Mispecified array
+    x = np.array([[12]])
+    print("BBB", x.dtype)
+    print("Handle the user mispecified array")
+    test_function(get_right_shape, x)
+
+    #6-Mispecified array
+    x = np.array([1000])
+    print("AAA", x.dtype)
+    #TODO-Convert the array into "DTYPE-float"
+    print('Simple one-dimensional array')
+    test_function(get_right_shape, x)
+
+    #7-Float
+    x = 1.
+    test_function(get_right_shape, x)
+
+    #8-Int
+    x = int(8)
+    test_function(get_right_shape, x)
+    out_x = get_right_shape(x)
     try:
-        out_x = get_right_shape(x)
-        print(type(out_x), out_x.shape)
+        out_x.dtype = np.float64
+        print("OK for changing the type")
+        print(out_x.dtype)
     except Exception as e:
-        print('CAUGHT', e)
+        print(e)
+    #9-Bool
+    x = True
+    test_function(get_right_shape, x)
+    x = False
+    test_function(get_right_shape, x)
+
+    #10-Tuple
+    x = (10,14)
+    test_function(get_right_shape, x)
+
+    #11-Tuple misp.
+    print("TEST with weird array")
+    x = (10, [145,26])
+    test_function(get_right_shape, x)
+    #out_x = get_right_shape(x)
+    #print(out_x, type(out_x))
+    #try:
+    #    out_x.dtype = np.float64
+    #except Exception as e:
+    #    print(e)
     
+    ######TODO-Does not work on everything, examples, this one.
+    #####Do we need to handle all of those cases ? 
+    ###Check the types of the array ? 
+    ###Future direction-create our own autodiff.array()?!
+    ### Future-Implement some functions in Cython ?
+
+
+
+
