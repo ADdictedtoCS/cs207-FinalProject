155a862694bd978fb3eaebed0b593956f94aad94
Theo-MAC <tguenais@g.harvard.edu>
2019-12-01 22:31:50 -0500

    Add config object into init

--- autodiff/__init__.py
+++ autodiff/__init__.py
@@ -1,3 +1,18 @@
 #Create __init__
 import numpy as np 
 import autodiff
+
+class Config:
+    def __init__(self, mode='forward'):
+        assert mode in ['forward', 'reverse'], "The mode should be either forward or reverse"
+        self.mode = mode
+        self.reverse_graph = [] if self.mode == 'reverse' else None
+    
+    def __repr__(self):
+        msg = "The current ad mode is {}. The reverse_graph attribute is set to {}".format(self.mode, self.reverse_graph)
+        return msg
+
+    #@classmethod
+    #def set_mode(self, mode):
+        
+config = Config()
