ba282e42d274f401048087e774b440418f9a3b4a
Theo-MAC <tguenais@g.harvard.edu>
2019-12-08 20:26:45 -0500

    Changes to optim

--- autodiff/optim.py
+++ autodiff/optim.py
@@ -73,7 +73,6 @@ class GradientDescent(Optimizer):
         #except: 
             #self.current_point -= self.lr * loss.grad
         
-
 class RMSProp(Optimizer):
     """
     #TODO-Add citation and explanations and so on. 
@@ -101,47 +100,7 @@ class RMSProp(Optimizer):
 #    def _step(self, loss):
 #        return NotImplementedError
 
-if __name__ == "__main__":
-    init_point = np.array([4,5])
-    try: 
-        import matplotlib.pyplot as plt 
-    except:
-        print("Please import matplotlib module if you want visualization.")
-    
-    def my_loss_fn(X):
-        x,y = X.unroll()
-        #if x.val < 0:
-        #    x=-x #force x to be positive. Problem with derivative though
-        #else:
-        #    pass
-        #return 50-x*x-2*y*y  
-        return x*y -y 
 
-    gd = Optimizer(0.01, 0.0001, my_loss_fn, init_point)
-    l = gd._eval(gd.current_point)
-    print(l)
-    u =init_point - l.grad
-    print(u)
-    try:
-        a_gd,l_gd,t_gd = gd.minimize(1000)
-    except Exception as e:
-        print(e)
-        print('Got into the exception')
-    gd = GradientDescent(0.01,-10e8, my_loss_fn, init_point)
-    a_gd, l_gd, t_gd = gd.minimize(10000)
-    #print('l',l,'\n') 
-    #print('t',t)
-    #plt.plot(l)
-    #plt.show()
-    #plt.plot(t)
-    #plt.show()
-    prop = RMSProp(0.01, -10e8, my_loss_fn, init_point, beta=0.9)
-    print(prop)
-    a, l, t = prop.minimize(10000)
-    plt.plot(l)
-    #plt.plot(l_gd, 'r')
-    #plt.plot(t)
-    plt.show()
 
 
     
